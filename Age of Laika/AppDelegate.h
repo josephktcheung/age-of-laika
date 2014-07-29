c2274935cee6ed0d9dff71ef66876d720bfd58ee
josephktcheung <josephktcheung@gmail.com>
2014-07-29 12:42:51 +0800

    Initial Commit

---
+++ Age of Laika/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Age of Laika
+//
+//  Created by Joseph Cheung on 29/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Age of Laika/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Age of Laika
+//
+//  Created by Joseph Cheung on 29/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
